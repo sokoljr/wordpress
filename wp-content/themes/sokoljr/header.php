b71722655ad3200f7065ffe8499e6256f710fc2e
Sergey <serg607.sokol@gmail.com>
2019-02-28 19:44:36 +0200

    Update header.php

--- wp-content/themes/sokoljr/header.php
+++ wp-content/themes/sokoljr/header.php
@@ -15,8 +15,11 @@
 <html lang="<?php echo get_locale(); ?>">
 <head>
     <title><?php echo wp_get_document_title(); ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
